020614424519107510c9db549e0d786632eb9ae6
tyehowitt <Tyehowitt@gmail.com>
2021-01-18 22:45:33 +0000

    final animation touch up navbar mobile

--- app.js
+++ app.js
@@ -2,16 +2,25 @@ const navSlide = ()=>{
     const burger = document.querySelector('.burger');
     const nav = document.querySelector('.nav-links');
     const navLinks = document.querySelectorAll('.nav-links li');
-    //Toggle Nav
+    
     burger.addEventListener('click',()=>{
+        //Toggle Nav
     nav.classList.toggle('nav-active');
 
+        //Animate links
+    navLinks.forEach((link, index) => {
+       if (link.style.animation) {
+        link.style.animation = ''
+       }else {
+           link.style.animation = `navLinkfade 0.5s ease forwards ${index / 7 + 2}s`;
+       }
     });
-    //Animate links
-    navLinks.forEach(link=>{
-    link.style.animation = `navLinkfade 0.5s ease forwards ${index / 7 + 2}s`;
-    console.log(index)
+      // Burger Animation
+      burger.classList.toggle('toggle');
+
+
     });
+
 }
 
 navSlide();
