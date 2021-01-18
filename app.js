a099729c1c2c50b0acc606d5754a387bfbdee1d6
tyehowitt <Tyehowitt@gmail.com>
2021-01-18 20:09:12 +0000

    javascript animation implementation (navbar-mobile)

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 const navSlide = ()=>{
     const burger = document.querySelector('.burger');
     const nav = document.querySelector('.nav-links');
-
+    const navLinks = document.querySelectorAll('.nav-links li');
+    //Toggle Nav
     burger.addEventListener('click',()=>{
     nav.classList.toggle('nav-active');
 
+    });
+    //Animate links
+    navLinks.forEach(link=>{
+    link.style.animation = `navLinkfade 0.5s ease forwards ${index / 7 + 2}s`;
+    console.log(index)
     });
 }
 
